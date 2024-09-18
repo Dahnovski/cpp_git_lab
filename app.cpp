da14c1a8e9299459ca77cd8f827e87efd440116b
Nazar <nazar.gavrilov20@gmail.com>
2024-09-18 22:35:12 +0300

    Ready start functionality

---
+++ app.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <regex>
+#include <string>
+#include <vector>
+
+#define         EXIT__SUCCESS           0
+
+int main()
+{
+        /// TODO
+        std::string text = "I saw a Mercedes C-Class and a Ford Mustang on the road. My friend drives a Toyota Camry.";
+        std::cout << text << std::endl;
+
+        return EXIT__SUCCESS;
+}
